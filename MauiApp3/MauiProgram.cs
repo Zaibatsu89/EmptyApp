f91b1a0f72404139678288d2a84ecba3976f3cf3
Rinse Cramer <rinsecramer@gmail.com>
2025-08-29 09:44:05 +0200

    feat: Implement initial HomePage with counter functionality This commit introduces the HomePage component, which serves as the main view for the application. The UI is constructed declaratively using MauiReactor and includes: A .NET bot image "Hello, World!" and "Welcome" labels An interactive counter button The component demonstrates basic state management with HomePageState. The button increments a counter on each click, and its text is dynamically updated to display the current count, showcasing how state changes trigger UI re-rendering.

---
+++ MauiApp3/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using MauiReactor;
+using MauiApp3.Components;
+using MauiApp3.Resources.Styles;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Hosting;
+
+namespace MauiApp3
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiReactorApp<HomePage>(app =>
+                {
+                    app.UseTheme<ApplicationTheme>();
+                },
+                unhandledExceptionAction: e => 
+                {
+                    System.Diagnostics.Debug.WriteLine(e.ExceptionObject);
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+
+            return builder.Build();
+        }
+    }
+}
